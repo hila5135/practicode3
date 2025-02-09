ecebb21d59b7d1b5310e26f54c6061a29ca3b161
hila5135 <hilabassis@gmail.com>
2025-02-09 21:51:35 +0200

    last

--- TodoApi1/Program.cs
+++ TodoApi1/Program.cs
@@ -1,76 +1,3 @@
-// using Microsoft.EntityFrameworkCore;
-// using TodoApi;
-// var builder = WebApplication.CreateBuilder(args);
-// //cors
-// builder.Services.AddCors(options =>
-// {
-//     options.AddDefaultPolicy(policy =>
-//     {
-//         policy.AllowAnyOrigin()
-//               .AllowAnyMethod()
-//               .AllowAnyHeader();
-//     });
-// });
-
-// //DbContext configuration
-// builder.Services.AddDbContext<ToDoDbContext>(options =>
-//     options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), 
-//                      ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ToDoDB"))));
-// //swagger
-// builder.Services.AddSwaggerGen();
-// builder.Services.AddEndpointsApiExplorer();
-
-// var app = builder.Build();
-
-// //using in cors
-// app.UseCors();
-
-// // Enable Swagger UI
-//     app.UseSwagger();
-//     app.UseSwaggerUI();  
-
-// // Define the API endpoints
-// //שליפה
-// app.MapGet("/items", async (ToDoDbContext db) =>
-// {
-//     var items = await db.Items.ToListAsync();
-//     return Results.Ok(items);
-// });
-// //עדכון
-// app.MapPut("/items/{id}", async (int id, Item updatedItem, ToDoDbContext db) =>
-// {
-//     var item = await db.Items.FindAsync(id);
-//     if(item == null){
-//           return Results.NotFound($"Item with ID {id} not found.");
-//     }
-//     item.Name = updatedItem.Name;
-//     item.IsComplete = updatedItem.IsComplete;
-//     //saving the item
-//     await db.SaveChangesAsync();
-//     return Results.Ok(item);
-// });
-
-// app.MapPost("/items" , async(Item updatedItem,ToDoDbContext db )=>
-// {
-//     await db.Items.AddAsync(updatedItem);
-//     await db.SaveChangesAsync();
-//     return Results.Ok(updatedItem);
-// });
-
-
-// app.MapDelete("/items/{id}" , async(int id ,ToDoDbContext db)=>
-// {
-//   var item = await db.Items.FindAsync(id);
-//     if (item == null)
-//     {
-//         return Results.NotFound($"Item with ID {id} not found.");
-//     }
-//     db.Items.Remove(item);  
-//     await db.SaveChangesAsync();  
-//     return Results.Ok(item);  
-// });
-// app.MapGet("/", () => "Todo List is running..");
-// app.Run();
 using Microsoft.EntityFrameworkCore;
 using TodoApi;
 
@@ -146,11 +73,4 @@ app.MapDelete("/items/{id}", async (int id, ToDoDbContext db) =>
 
 app.MapGet("/", () => "Todo List is running..");
 
-//lastLevel
-// import renderApi from '@api/render-api';
-
-// renderApi.auth('rnd_bjNQ7Hz3he3zIx7Nq4QZtsWGeYay');
-// renderApi.listServices({includePreviews: 'true', limit: '20'})
-//   .then(({ data }) => console.log(data))
-//   .catch(err => console.error(err));
 app.Run();
