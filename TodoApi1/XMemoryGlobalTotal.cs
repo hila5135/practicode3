dcb20ad0e0945b854a918ae31d55ca1447a77a2f
hila5135 <hilabassis@gmail.com>
2025-02-09 17:11:37 +0200

    final

---
+++ TodoApi1/XMemoryGlobalTotal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApi
+{
+    public partial class XMemoryGlobalTotal
+    {
+        public decimal? TotalAllocated { get; set; }
+    }
+}
